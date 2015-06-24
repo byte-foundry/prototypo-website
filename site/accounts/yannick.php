8558f2a7d963f4e723faeccdac7ea0714734ccdd
yannick mathey <hello@yannickmathey.com>
2015-06-24 17:38:33 +0200

    Change wording newsletter

--- site/accounts/yannick.php
+++ site/accounts/yannick.php
@@ -8,4 +8,6 @@ password: >
   $2a$10$1ppnHV7bSBZspNH.knE.SupuTe6kwqAcAtMXWFXBW6ou.hMHdyJee
 language: en
 role: admin
-history: [ ]
+history:
+  - home
+token: 9ee7c8595c7e6583634e5bb44add0071f857b73c
