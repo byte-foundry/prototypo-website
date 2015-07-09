223ff1a4cd63925aabfa71a74e3d47be78ea0aa1
kevinvennitti <kevin.vennitti@gmail.com>
2015-07-09 11:40:43 +0200

    Update Backers' url

--- site/snippets/backers.php
+++ site/snippets/backers.php
@@ -7,15 +7,17 @@
 ?>
 
   <li class="AwesomeBacker">
-    <?php if ($backer->url != '') : ?>
-      <a href="<?php echo $backer->url; ?>" target="_blank">
+    <?php if ($backer->url != '') : 
+      $urlBefore = (substr($backer->url,0,7) != 'http://' && substr($backer->url,0,8) != 'https://') ? 'http://' : '';?>
+      <a href="<?php echo $urlBefore.$backer->url; ?>" target="_blank">
     <?php endif; ?>
     
     <img class="AwesomeBacker-avatar" src="<?php echo $backer->avatar == '' ? url('assets/img/pictos/pictoMissingAvatar.svg') : $backer->avatar; ?>">
     <div class="AwesomeBacker-name textType-txt textSize-txt-medium colorBrightest"><?php echo $backer->name; ?></div>
     
-    <?php if ($backer->url != '') : ?>
-      <a class="AwesomeBacker-url textType-txt textSize-txt-xsmall colorDarkGray" href="<?php echo $backer->url; ?>" target="_blank"><?php echo $backer->url; ?></a>
+    <?php if ($backer->url != '') : 
+    ?>
+      <a class="AwesomeBacker-url textType-txt textSize-txt-xsmall colorDarkGray" href="<?php echo $urlBefore.$backer->url; ?>" target="_blank"><?php echo $backer->url; ?></a>
     <?php endif; ?>
     
     <?php if ($backer->url != '') : ?>
