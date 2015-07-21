fb81634c96b3378285c77ffd9d320e226d84e906
yannick mathey <hello@yannickmathey.com>
2015-07-21 11:24:42 +0200

    resolve conflicts

--- site/accounts/yannick.php
+++ site/accounts/yannick.php
@@ -10,12 +10,4 @@ language: en
 role: admin
 history:
   - home
-  - faq/when-the-v1-0-will-be-available
-  - null
-  - home
-  - blog/i-didnt-shoot-the-serif
-  - blog/prototypo-update-4-overcoming-dilemmas
-  - blog/article
-  - blog
-  - blog/prototypo-update-1
 token: 69bc9e54d930dd48533458a4b83eb165cefb5bd1
