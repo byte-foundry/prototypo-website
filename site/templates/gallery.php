37c381b39b21842ff9d5e602c2c6bc58ebd4b05e
= <lrbabe@gmail.com>
2015-07-27 17:34:12 +0200

    commit modified statify.php

--- site/templates/gallery.php
+++ site/templates/gallery.php
@@ -3,7 +3,7 @@
   <main class="PageContent Gallery" role="main">
 
     <div class="fitToContent">
-      
+
   		<header class="PageHeader text-center fitToContent">
     		<h1 class="textType-title textSize-title-large colorDarkest"><?php echo $page->section1Title()->kirbytext(); ?></h1>
         <h3 class="textType-subtitle textSize-title-small colorDark"><?php echo $page->section1Subtitle()->kirbytext(); ?></h3>
@@ -11,29 +11,29 @@
 
   		<div class="Section-wrapTxt textType-txt textSize-txt-large marginTop30 colorDarkest fitToContent">
     		<?php echo $page->section1Txt()->kirbytext(); ?>
-    		
+
     		<?php $fonts = yaml($page->fonts()); ?>
-    		
+
     		<style>
-      		<?php foreach($fonts as $i => $f) : if (!empty($f[fontfile])): ?>
+      		<?php foreach($fonts as $i => $f) : if (!empty($f['fontfile'])): ?>
       	  @font-face {
         	  font-family: 'Font<?php echo $i; ?>';
-            src: url('<?php echo url('/content/'.$page->dirname().'/'.$f[fontfile]); ?>');
-      	  }	
+            src: url('<?php echo url('/content/'.$page->dirname().'/'.$f['fontfile']); ?>');
+      	  }
       	  <?php endif; endforeach; ?>
         </style>
-        
+
         <ul class="Fonts small-block-grid-1 medium-block-grid-3 large-block-grid-3">
         <?php foreach($fonts as $i => $f) : ?>
           <li class="FontsItem text-center">
             <div class="FontsItemWrap radius">
-              <div class="FontsView colorDarkest" style="font-family:'Font<?php echo $i; ?>' , inherit;font-size:<?php echo $f[fontsize]; ?>px; line-height:<?php echo $f[fontlineheight]; ?>px"><?php echo $f[fontstring]; ?></div>
-              <h6 class="FontsName textType-subtitle textSize-txt-xlarge colorDarkest"><?php echo $f[fontname]; ?></h6>
-              <div class="FontsNumberVariants"><?php echo $f[fontnumbervariants]; ?> variant<?php if (intval($f[fontnumbervariants]) > 1) echo 's'; ?></div>
+              <div class="FontsView colorDarkest" style="font-family:'Font<?php echo $i; ?>' , inherit;font-size:<?php echo $f['fontsize']; ?>px; line-height:<?php echo $f['fontlineheight']; ?>px"><?php echo $f['fontstring']; ?></div>
+              <h6 class="FontsName textType-subtitle textSize-txt-xlarge colorDarkest"><?php echo $f['fontname']; ?></h6>
+              <div class="FontsNumberVariants"><?php echo $f['fontnumbervariants']; ?> variant<?php if (intval($f['fontnumbervariants']) > 1) echo 's'; ?></div>
               <!--
               <div class="FontsInfos">
-                <div class="FontsDescription textType-subtxt textSize-txt-medium colorDark"><?php echo $f[fontdescription]; ?></div>
-                <a href="<?php if (!empty($f[fontfile])) echo url('/content/'.$page->dirname().'/'.$f[fontfile]) ?>" 
+                <div class="FontsDescription textType-subtxt textSize-txt-medium colorDark"><?php echo $f['fontdescription']; ?></div>
+                <a href="<?php if (!empty($f['fontfile'])) echo url('/content/'.$page->dirname().'/'.$f['fontfile']) ?>" 
                    class="callToAction FontsDownload text-center">
                   <?php echo $page->downloadButtonLabel()->kirbytext(); ?>
                 </a>
@@ -44,12 +44,12 @@
         <?php endforeach; ?>
         </ul>
   		</div>
-      
+
 
   		<div class="Section-wrapTxt textType-txt marginTop30 colorBrightest text-center">
         <?php echo $page->section1TxtAfter()->kirbytext(); ?>
   		</div>
-      
+
     </div>
 
   </main>
--- site/templates/pricing.php
+++ site/templates/pricing.php
@@ -16,7 +16,7 @@
     		</div>
 
         <div class="PricingItemFree-wrapIn">
-          <div class="PricingItemFree-pack textType-txt text-center textSize-txt-large colorWhite"><?php echo nl2br($pack[packservices]); ?></div>
+          <div class="PricingItemFree-pack textType-txt text-center textSize-txt-large colorWhite"><?php echo nl2br($pack['packservices']); ?></div>
         </div>
 
     		<form class="Newsletter text-center">
@@ -53,38 +53,38 @@
     		<?php
       		$packs = yaml($page->packs());
       		foreach($packs as $id => $pack) :
-      		  $state = ($pack[packstate] == 'notavailable') ? 'disabled' : '';
+      		  $state = ($pack['packstate'] == 'notavailable') ? 'disabled' : '';
       	?>
 
         <li class="PricingItem">
           <div class="PricingItem-wrap <?php echo $state; ?>">
-            <h3 class="PricingItem-packtitle textType-txt textSize-txt-small"><?php echo $pack[packname]; ?></h3>
+            <h3 class="PricingItem-packtitle textType-txt textSize-txt-small"><?php echo $pack['packname']; ?></h3>
             <h3 class="PricingItem-title textType-txt textSize-title-small <?php if ($state == 'disabled') echo 'colorGray'; else echo 'colorSecondBackgroundColor'; ?>">
-            <?php if ($pack[packprice] === '') : ?>
+            <?php if ($pack['packprice'] === '') : ?>
               <img src="<?php echo url('assets/img/Enterprise.svg'); ?>">
             <?php else : ?>
               <span class="PricingItem-priceBefore textSize-txt-large">$</span><!--
            --><span class="PricingItem-price textSize-title-xlarge">
 <!--
-                <span class="js_annualBilling_price"><?php echo $pack[packprice2]; ?></span>
-                <span class="js_monthlyBilling_price"><?php echo $pack[packprice]; ?></span>
+                <span class="js_annualBilling_price"><?php echo $pack['packprice2']; ?></span>
+                <span class="js_monthlyBilling_price"><?php echo $pack['packprice']; ?></span>
 -->
-                <div class="js_price"><?php echo $pack[packprice2]; ?><br><?php echo $pack[packprice]; ?></div>
+                <div class="js_price"><?php echo $pack['packprice2']; ?><br><?php echo $pack['packprice']; ?></div>
               </span><!--
            --><span class="PricingItem-priceAfter textSize-txt-medium">/mo.</span>
             <?php endif; ?>
             </h3>
 
 <!--
-            <h6 class="PricingItem-price textType-txt textSize-txt-small colorDarkGray">Monthly billing: <?php echo $pack[packprice]; ?></h6>
+            <h6 class="PricingItem-price textType-txt textSize-txt-small colorDarkGray">Monthly billing: <?php echo $pack['packprice']; ?></h6>
 
 -->
             <div class="PricingItem-infos textType-subtxt colorDarkGray">
-              <div class="PricingItem-pack textType-txt textSize-txt-medium"><?php echo nl2br($pack[packservices]); ?></div>
-              <div class="PricingItem-pack textType-txt textSize-txt-medium"><?php echo nl2br($pack[packservices2]); ?></div>
-<!--               <div class="PricingItem-content textType-txt textSize-txt-small"><?php echo nl2br($pack[packdescription]); ?></div> -->
+              <div class="PricingItem-pack textType-txt textSize-txt-medium"><?php echo nl2br($pack['packservices']); ?></div>
+              <div class="PricingItem-pack textType-txt textSize-txt-medium"><?php echo nl2br($pack['packservices2']); ?></div>
+<!--               <div class="PricingItem-content textType-txt textSize-txt-small"><?php echo nl2br($pack['packdescription']); ?></div> -->
               <div class="PricingItem-getStarted">
-                <div class="callToAction <?php echo $state; ?>"><?php echo $pack[packbuttonlabel]; ?></div>
+                <div class="callToAction <?php echo $state; ?>"><?php echo $pack['packbuttonlabel']; ?></div>
               </div>
             </div>
           </div>
