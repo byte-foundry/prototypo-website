560c94da602c65c11f7c41879e959bb03a5de3e4
yannickmathey <hello@yannickmathey.com>
2015-10-06 22:51:43 +0200

    adroll

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -20,5 +20,25 @@
 
 <script type="text/javascript" src="https://api.taxamo.com/js/v1/taxamo.all.nojquery.js"></script>
 
+<script type="text/javascript">
+   adroll_adv_id = "TTP725TQI5FEZIX2X4TIUJ";
+   adroll_pix_id = "7GNFXBZIR5G5VDEFAEZAK3";
+   (function () {
+       var _onload = function(){
+           if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+           if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+           var scr = document.createElement("script");
+           var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+           scr.setAttribute('async', 'true');
+           scr.type = "text/javascript";
+           scr.src = host + "/j/roundtrip.js";
+           ((document.getElementsByTagName('head') || [null])[0] ||
+               document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+       };
+       if (window.addEventListener) {window.addEventListener('load', _onload, false);}
+       else {window.attachEvent('onload', _onload)}
+   }());
+</script>
+
 </body>
 </html>
