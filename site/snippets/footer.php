d66fa312edda5908c9c3feb29456a91c8bcccf63
Louis-Rémi Babé <lrbabe@gmail.com>
2016-03-18 16:14:43 +0100

    Serve Google's conversion tracking script over https

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -50,12 +50,6 @@ var google_custom_params = window.google_tag_params;
 var google_remarketing_only = true;
 /* ]]> */
 </script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/952279521/?value=0&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
+<script type="text/javascript" src="https://www.googleadservices.com/pagead/conversion.js"></script>
 </body>
 </html>
