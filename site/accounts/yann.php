27dfd0be0da6925e09dc52facf1ddf6345980f4a
Julien Barret <ibarretj@gmail.com>
2017-08-22 14:29:43 +0200

    Revert "Merge branch 'develop'"
    This reverts commit 0a12b907dd920ca51ad8a84f15c16452b1d192a2, reversing
    changes made to 5a8bbc891c302563e27705e18ef95e11a484a2fd.

--- site/accounts/yann.php
+++ site/accounts/yann.php
@@ -9,12 +9,12 @@ password: >
 language: fr
 role: admin
 history:
-  - press
-  - >
-    press/creating-a-designer-font-is-time-consuming-and-difficult-this-french-startup-has-made-the-whole-process-as-simple-as-a-couple-of-clicks
   - >
     blog/introducing-spectral-the-first-parametric-google-font-by-prototypo
+  - press
   - >
     press/prototypo-levoluzione-della-tipografia
   - >
     press/prototypo-will-let-anyone-design-original-fonts
+  - >
+    press/revised-editing-tool-promises-to-revolutionise-font-creation
