00938ead7b2d53c1de7d5732f94c1c1c7243d10e
Julien Barret <ibarretj@gmail.com>
2017-11-30 17:37:47 +0100

    fix/smashing blog post

--- site/snippets/dev/scripts.php
+++ site/snippets/dev/scripts.php
@@ -4,6 +4,7 @@
 <script src="/assets/js/prototypo-canvas.js"></script>
 <script src="/assets/js/font.js"></script>
 <script src="/assets/js/main.js"></script>
+<script src="/assets/js/prism.js"></script>
 <script src="/assets/js/analytics.js"></script>
 
 <script src="/assets/js/demo.js"></script>
